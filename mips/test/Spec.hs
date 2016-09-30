ef0ccf05ed77ed5c7fd9d483338dec7c5e6a7f58
Hogeyama <gan13027830@gmail.com>
2016-09-30 21:08:30 +0900

    update test

--- mips/test/Spec.hs
+++ mips/test/Spec.hs
@@ -18,13 +18,42 @@ import Simm     (simm)
 import Emit     (emit)
 
 import Prelude hiding (lex)
-import Control.Monad (forM_, when)
-import System.IO (readFile, withFile, IOMode(..))
-import qualified Shelly as Sh
-import Shelly (shelly, run, silently, rm, fromText, (</>), (<.>), liftIO, Sh)
-import Data.Text (Text, pack)
-import qualified Data.Text.IO as TIO
-default (Text)
+import System.IO (withFile, IOMode(..))
+import Control.Monad.IO.Class (liftIO)
+--import Control.Monad (forM_, when)
+--import qualified Shelly as Sh
+--import Shelly (shelly, run, silently, rm, fromText, (</>), (<.>), liftIO, Sh)
+--import Data.Text (Text, pack)
+--import qualified Data.Text.IO as TIO
+--default (Text)
+
+main :: IO ()
+main =
+  unit "test/fib" >>= \case
+    Right () -> return ()
+    Left e -> print e
+
+
+unit :: FilePath -> IO (Either Error ())
+unit f = do
+  s <- readFile (f ++ ".ml")
+  withFile (f ++ ".s") WriteMode $ \out ->
+    runCamlDefault $ do
+      tks <- lex s
+      e <- parse tks
+      e <- typing e
+      e <- kNormalize e
+      e <- alpha e
+      e <- optimise e
+      e <- closureConvert e
+      e <- virtualCode e
+      liftIO $ putStrLn "" >> print e
+      e <- simm e
+      liftIO $ putStrLn "" >> print e
+      e <- regAlloc e
+      liftIO $ putStrLn "" >> print e
+      emit out e
+
 
 targets :: [FilePath]
 targets = [
@@ -59,57 +88,3 @@ targets = [
 --  , "sum"
 --  , "sum-tail"
   ]
-
-main :: IO ()
-main = do
-  error "not implemented"
-  --mapM_ test targets
-  --putStrLn $ "\n\tcompleted " ++ show (length targets) ++ " tests"
-
-test :: FilePath -> IO ()
-test f = do
-  putStrLn $ "[[" ++ f ++ "]]"
-  m <- unit $ "test/" ++ f
-  case m of
-    Right _ -> shelly $ do
-      res <- exeMinCaml $ "test/" ++  f ++ "-res"
-      ans <- exeOCaml   $ "test/" ++  f
-      liftIO $ do
-        putStr "result: " >> TIO.putStr res
-        putStr "answer: " >> TIO.putStr ans
-      when (res/=ans) $ error f
-    Left e -> error $ f ++ ": " ++ show e
-
-exeMinCaml :: FilePath -> Sh Text
-exeMinCaml s = silently $ run "gcc" options >> (run outp [] <* rm outp)
-  where
-    options = ["-g", "-O2", "-Wall", "-m32"] ++ sources ++ ["-lm", "-o", out]
-    sources = map pack [s ++ ".s", "libmincaml.S", "stub.c"]
-    out  = pack s
-    outp = "." </> s
-
-exeOCaml :: FilePath -> Sh Text
-exeOCaml s = silently $ run "ocamlc" options >> (run outp [] <* rm outp)
-  where
-    options = [source, "-o", out]
-    source = pack $ s ++ ".ml"
-    outp = "." </> s
-    out  = pack s
-
-unit :: FilePath -> IO (Either Error ())
-unit f = do
-  s <- readFile (f ++ ".ml")
-  withFile (f ++ "-res.s") WriteMode $ \out ->
-    runCamlDefault $ lex s
-    >>= parse
-    >>= typing
-    >>= kNormalize
-    >>= alpha
-    >>= optimise
-    >>= closureConvert
-    >>= virtualCode
-    >>= simm
-    >>= regAlloc
-    -- >>= (\e -> liftIO (print e) >> return e)
-    >>= emit out
-
