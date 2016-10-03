40759c4b018161c5393350f9db5abedd356e4e2f
Hogeyama <gan13027830@gmail.com>
2016-10-03 20:36:59 +0900

    add libmincaml.s

---
+++ mips/libmincaml.s
@@ -0,0 +1,22 @@
+
+min_caml_print_newline:
+	li	$a0, 10
+	li	$v0, 11
+	syscall
+	jr	$ra
+min_caml_print_int: #$v0
+	move	$a0, $v0
+	li	$v0, 1
+	syscall
+	jr	$ra
+min_caml_print_double: #$f0
+	mov.d	$f12, $f0
+	li	$v0, 3
+	syscall
+	jr	$ra
+min_caml_truncate:
+	trunc.w.d $f0, $f0
+	mfc1	$v0, $f0
+	jr	$ra
+
+
