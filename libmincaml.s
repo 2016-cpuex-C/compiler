e8bd6f320a195f22be194e2c5af95142166eeabd
Hogeyama <gan13027830@gmail.com>
2016-10-22 21:59:23 +0900

    add libmincaml.s

---
+++ libmincaml.s
@@ -0,0 +1,172 @@
+
+##################
+## libmincaml.s ##
+##################
+
+min_caml_print_newline:
+	li	$v0, 10
+	print_c	$v0
+	jr	$ra
+
+min_caml_print_int: # $v0
+	print_i	$v0
+	jr	$ra
+
+min_caml_print_char: # $v0
+	print_c	$v0
+	jr	$ra
+
+min_caml_print_float:
+	print_f	$f0
+	jr	$ra
+
+min_caml_read_int:
+	read_i	$v0
+	jr	$ra
+
+min_caml_read_float:
+	read_f	$f0
+	jr	$ra
+
+
+min_caml_create_array: # array of length $v0, initialized by $v1
+	move	$a0, $v0
+	move	$v0, $gp
+create_array_loop:
+	li	$a2, 0
+	bne	$a0, $a2, create_array_cont
+create_array_exit:
+	jr	$ra
+create_array_cont:
+	sw	$v1, 0($gp)
+	addi	$a0, $a0, -1
+	addi	$gp, $gp, 4
+	j	create_array_loop
+#
+min_caml_create_float_array: # array of length $v0, initialized by $f0
+	move	$a0, $v0
+	move	$v0, $gp
+create_float_array_loop:
+	li	$a2, 0
+	bne	$a0, $a2, create_float_array_cont
+create_float_array_exit:
+	jr	$ra
+create_float_array_cont:
+	s.s	$f0, 0($gp)
+	addi	$a0, $a0, -1
+	addi	$gp, $gp, 4
+	j	create_float_array_loop
+
+min_caml_int_of_float: # f0 -> v0
+	ftoi	$v0, $f0
+	jr	$ra
+
+min_caml_truncate:
+	j	min_caml_float_of_int
+min_caml_float_of_int: # v0 -> f0
+	itof	$f0, $v0
+	jr	$ra
+
+min_caml_floor: # f0 -> f0
+	floor $f0, $f0
+	jr	$ra
+
+min_caml_sqrt: # f0 -> f0
+	sqrt	$f0, $f0
+	jr	$ra
+
+min_caml_fiszero: # f0 -> v0
+	l.sl	$f1, const_f_zero
+	c.eq.s	$f0, $f1, min_caml_fiszero_t
+	l.sl	$f1, const_f_zero_neg
+	c.eq.s	$f0, $f1, min_caml_fiszero_t
+	li	$v0, 0 # false
+	jr	$ra
+min_caml_fiszero_t:
+	li	$v0, 1 # true
+	jr	$ra
+
+min_caml_fispos: # f0 -> v0
+	l.sl	$f1, const_f_zero
+	c.lt.s	$f1, $f0, min_caml_fispos_t
+	li	$v0, 0 # false
+	jr	$ra
+min_caml_fispos_t:
+	li	$v0, 1 # true
+	jr	$ra
+
+min_caml_fisneg: # f0 -> v0
+	l.sl	$f1, const_f_zero
+	c.lt.s	$f0, $f1, min_caml_fispos_t
+	li	$v0, 0 # false
+	jr	$ra
+min_caml_fisneg_t:
+	li	$v0, 1 # true
+	jr	$ra
+
+min_caml_xor: # v0, v1 -> v0
+	li	$a0, 0
+	beq	$v0, $a0, min_caml_xor_2
+	# v0=t
+	beq	$v1, $a0, min_caml_xor_1
+	# v0=t, v1=t
+	li	$v0, 0
+	jr	$ra
+min_caml_xor_1:
+	# v0=t, v1=f
+	li	$v0, 1
+	jr	$ra
+min_caml_xor_2:
+	# v0=f
+	beq	$v1, $a0, min_caml_xor_3
+	# v0=f, v1=t
+	li	$v0, 1
+	jr	$ra
+min_caml_xor_3:
+	# v0=f, v1=f
+	li	$v0, 0
+	jr	$ra
+
+min_caml_fless: # f0,f1 -> v0
+	c.lt.s	$f0, $f1, min_caml_fless_true
+	li	$v0, 0
+	jr	$ra
+min_caml_fless_true:
+	li	$v0, 1
+	jr	$ra
+
+min_caml_fneg: # f0 -> f0
+	l.sl	$f1, const_f_zero
+	sub.s	$f0, $f1, $f0
+	jr	$ra
+
+min_caml_fabs: # f0 -> f0
+	l.sl	$f1, const_f_zero
+	c.lt.s	$f0, $f1, min_caml_fneg
+	jr	$ra
+
+min_caml_fsqr: # f0 -> f0
+	mul.s	$f0, $f0, $f0
+	jr	$ra
+
+min_caml_fhalf: # f0 -> f0
+	l.sl	$f1, const_f_half
+	mul.s	$f0, $f0, $f1
+	jr	$ra
+
+###############
+### sin, cos ##
+###############
+
+min_caml_sin:
+	sin	$f0, $f0
+	jr	$ra
+min_caml_cos:
+	cos	$f0, $f0
+	jr	$ra
+min_caml_atan:
+	atan	$f0, $f0
+	jr	$ra
+
+
+
