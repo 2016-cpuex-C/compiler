01f54cafefc58701da8a07a3075efcddfd5a826f
Hogeyama <gan13027830@gmail.com>
2017-01-11 18:37:04 +0900

    improve inlining

--- src/MiddleEnd/Inline.hs
+++ src/MiddleEnd/Inline.hs
@@ -11,9 +11,15 @@ import           MiddleEnd.KNormal
 import qualified MiddleEnd.Alpha as Alpha
 
 import qualified Data.Map as M
+import qualified Data.Set as S
+import Data.List (isPrefixOf)
+
+--import Debug.Trace (trace)
 
 inline :: KExpr -> Caml KExpr
-inline = g M.empty
+inline e = do
+  {-limit <- use threshold-}
+  inlineSub 2000 e
 
 size :: KExpr -> Int
 size = \case
@@ -24,25 +30,72 @@ size = \case
   KLetTuple _ _ e             -> 1 + size e
   _ -> 1
 
-g :: Map Id ([(Id,Type)], KExpr) -> KExpr -> Caml KExpr
-g env e = case e of
-  KIfEq x y e1 e2 -> KIfEq x y <$> g env e1 <*> g env e2
-  KIfLe x y e1 e2 -> KIfLe x y <$> g env e1 <*> g env e2
+inlineSub :: Int -> KExpr -> Caml KExpr
+inlineSub _ = g M.empty
+  where
+    g :: Map Id ([(Id,Type)], KExpr) -> KExpr -> Caml KExpr
+    g env e = case e of
+      KIfEq x y e1 e2 -> KIfEq x y <$> g env e1 <*> g env e2
+      KIfLe x y e1 e2 -> KIfLe x y <$> g env e1 <*> g env e2
 
-  KLet xt e1 e2 -> KLet xt <$> g env e1 <*> g env e2
-  KLetRec (KFunDef (x,t) yts e1) e2 -> do
-    limit <- use threshold
-    let env' = if size e1 > limit then env else M.insert x (yts,e1) env
-    e1' <- g env' e1
-    e2' <- g env' e2
-    return $ KLetRec (KFunDef (x,t) yts e1') e2'
-  KApp x ys ->
-    case M.lookup x env of
-      Just (zts,e') -> do
-        ($logInfo) $ "inlining " <> pack x
-        let env' = M.fromList (zip (map fst zts) ys)
-        Alpha.g env' e'
+      KLet xt e1 e2 -> KLet xt <$> g env e1 <*> g env e2
+      KLetRec f@(KFunDef (x,t) yts e1) e2 -> do
+        ($logDebug) $ "sizeof " <> pack x <> ": " <> show' (size e1, isRecursive f)
+        --let inlining = size e1 < limit && (not (isRecursive f))
+        let inlining = not (isRecursive f)
+            env' = if not inlining then env else M.insert x (yts,e1) env
+        e1' <- g env' e1
+        e2' <- g env' e2
+        return $ KLetRec (KFunDef (x,t) yts e1') e2'
+      KApp x ys ->
+        case M.lookup x env of
+          Just (zts,e') -> do
+            ($logInfo) $ "inlining " <> pack x
+            let env' = M.fromList (zip (map fst zts) ys)
+            Alpha.g env' e'
+          _ -> return e
+      KLetTuple xts y e' -> KLetTuple xts y <$> g env e'
       _ -> return e
-  KLetTuple xts y e' -> KLetTuple xts y <$> g env e'
-  _ -> return e
+
+
+-- recursive functionのみinline化
+inlineRec :: KExpr -> Caml KExpr
+inlineRec e = do
+  limit <- use threshold
+  inlineRecSub limit e
+
+inlineRecSub :: Int -> KExpr ->Caml KExpr
+inlineRecSub limit = g' Nothing M.empty
+  where
+    g' :: Maybe Id -> Map Id ([(Id,Type)], KExpr) -> KExpr -> Caml KExpr
+    g' target env e =
+      let g = g' target
+      in  case e of
+      KIfEq x y e1 e2 -> KIfEq x y <$> g env e1 <*> g env e2
+      KIfLe x y e1 e2 -> KIfLe x y <$> g env e1 <*> g env e2
+
+      KLet xt e1 e2 -> KLet xt <$> g env e1 <*> g env e2
+      KLetRec (KFunDef (x,t) yts e1) e2 -> do
+        let inlining = size e1 < limit
+                    && not ("read_net_item"   `isPrefixOf` x)
+                    && not ("read_or_network" `isPrefixOf` x)
+                    -- 350でcolort tarinai 他のは1500いける
+            env' = if not inlining then env else M.insert x (yts,e1) env
+        e1' <- g' (Just x) env' e1
+        e2' <- g env' e2
+        return $ KLetRec (KFunDef (x,t) yts e1') e2'
+      KApp x ys ->
+        case (target,M.lookup x env) of
+          (Just x', Just (zts,e')) | x==x' -> do
+            ($logInfo) $ "inlining " <> pack x <> " size: " <> show' (size e')
+            let env' = M.fromList (zip (map fst zts) ys)
+            Alpha.g env' e'
+          _ -> return e
+      KLetTuple xts y e' -> KLetTuple xts y <$> g env e'
+      _ -> return e
+
+-- even-oddみたいな相互再帰には対応していない
+-- レイトレにはないのでまあ
+isRecursive :: KFunDef -> Bool
+isRecursive (KFunDef (x,_) _ e) = x `S.member` fv e
 
--- src/MiddleEnd/Optimise.hs
+++ src/MiddleEnd/Optimise.hs
@@ -10,19 +10,32 @@ import MiddleEnd.Assoc
 import MiddleEnd.Inline
 import MiddleEnd.ConstFold
 import MiddleEnd.Elim
-import MiddleEnd.CSE
+import MiddleEnd.CSE ()
 import MiddleEnd.StaticAlloc
 
 import Prelude hiding (log)
 
 optimise :: KExpr -> Caml KExpr
-optimise e = use optimiseLimit >>= go e
+optimise = optimise1 >=> optimise2
+
+optimise1 :: KExpr -> Caml KExpr
+optimise1 e = use optimiseLimit >>= go e
   where
     go e' 0 = return e'
     go e' n = do
       ($logInfo) $ "iter: " <> show' n
-      e'' <- staticAlloc =<< cse =<< elim =<< constFold =<< inline =<< assoc =<< beta e'
+      e'' <- staticAlloc =<< elim =<< constFold =<< inline =<< assoc =<< beta e'
       if e'==e''
         then return e'
         else go e'' (n-1)
 
+optimise2 :: KExpr -> Caml KExpr
+optimise2 e = ($logInfo) "optimise2" >> use optimiseLimit >>= go e
+  where
+    go e' 0 = return e'
+    go e' n = do
+      ($logInfo) $ "iter: " <> show' n
+      e'' <- staticAlloc =<< elim =<< constFold =<< inlineRec =<< assoc =<< beta e'
+      if e'==e''
+        then return e'
+        else go e'' (n-1)
