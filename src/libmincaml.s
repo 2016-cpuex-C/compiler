0edde2f8b6204e0131ac412cec14e4eaedd406ff
Hogeyama <gan13027830@gmail.com>
2017-02-09 16:41:15 +0900

    rm assembly function which uses virtual instructions

--- src/libmincaml.s
+++ src/libmincaml.s
@@ -3,22 +3,22 @@
 ## libmincaml.s ##
 ##################
 
-min_caml_print_newline:
-	li	$v0, 10
-	print_c	$v0
-	jr	$ra
+#min_caml_print_newline:
+#	li	$v0, 10
+#	print_c	$v0
+#	jr	$ra
 
-min_caml_print_int: # $v0
-	print_i	$v0
-	jr	$ra
+#min_caml_print_int: # $v0
+#	print_i	$v0
+#	jr	$ra
 
 min_caml_print_char: # $v0
 	print_c	$v0
 	jr	$ra
 
-min_caml_print_float:
-	print_f	$f0
-	jr	$ra
+#min_caml_print_float:
+#	print_f	$f0
+#	jr	$ra
 
 min_caml_read_int:
 	read_i	$v0
@@ -27,7 +27,6 @@ min_caml_read_int:
 min_caml_read_float:
 	read_f	$f0
 	jr	$ra
-
 
 min_caml_array_init:	# write $a0 in ($v0)~($v0+$v1-1), return $v0 itself
 			# a0: elem, v0: init addr, v1: cnt
@@ -85,41 +84,38 @@ min_caml_create_float_array_cont:
 	addi	$gp, $gp, 1
 	j	min_caml_create_float_array_loop
 
-min_caml_truncate:
-	j	min_caml_int_of_float
-min_caml_int_of_float: # f0 -> v0
-	ftoi	$v0, $f0
-	jr	$ra
+###############################
+### use virtual instructions ##
+###############################
 
-min_caml_float_of_int: # v0 -> f0
-	itof	$f0, $v0
-	jr	$ra
-
-min_caml_floor: # f0 -> f0
-	floor $f0, $f0
-	jr	$ra
-
-min_caml_sqrt: # f0 -> f0
-	sqrt	$f0, $f0
-	jr	$ra
-
-min_caml_fsqr: # f0 -> f0
-	mul.s	$f0, $f0, $f0
-	jr	$ra
-
-###############
-### sin, cos ##
-###############
-
-min_caml_sin:
-	sin	$f0, $f0
-	jr	$ra
-
-min_caml_cos:
-	cos	$f0, $f0
-	jr	$ra
-
-min_caml_atan:
-	atan	$f0, $f0
-	jr	$ra
+#min_caml_truncate:
+#	j	min_caml_int_of_float
+#min_caml_int_of_float: # f0 -> v0
+#	ftoi	$v0, $f0
+#	jr	$ra
+#
+#min_caml_float_of_int: # v0 -> f0
+#	itof	$f0, $v0
+#	jr	$ra
+#
+#min_caml_floor: # f0 -> f0
+#	floor $f0, $f0
+#	jr	$ra
+#
+#min_caml_sqrt: # f0 -> f0
+#	sqrt	$f0, $f0
+#	jr	$ra
+#
+#
+#min_caml_sin:
+#	sin	$f0, $f0
+#	jr	$ra
+#
+#min_caml_cos:
+#	cos	$f0, $f0
+#	jr	$ra
+#
+#min_caml_atan:
+#	atan	$f0, $f0
+#	jr	$ra
 
